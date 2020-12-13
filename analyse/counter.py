05e39754aa6cd6cedbc7bd7b42d673515aefd99b
tijmen104 <tijmen104@gmail.com>
2020-12-13 12:21:48 +0100

    add analyser (JSON based)

---
+++ analyse/counter.py
@@ -0,0 +1,89 @@
+import json
+import re
+import nltk
+from nltk import bigrams
+from nltk.tokenize.toktok import ToktokTokenizer
+import heapq
+
+class Counter:
+
+    def __init__(self):
+        self.articles =[]
+        self.all_text = ""
+
+    def add_sites(self, sites):
+        for site in sites:
+            with open('../scraping/sitemapURLs/' + site, 'r') as f:
+                article = json.load(f)
+                for txt in article:
+                    txt = txt['full_text']
+                    self.add_article(txt)
+
+    def add_article(self, text):
+        txt = self.remove_special_characters(text)
+        clean_txt = self.remove_stopwords(txt, is_lower_case=True)
+        self.articles.append(clean_txt)
+        self.all_text+=clean_txt
+
+    def get_articles(self):
+        return self.articles
+
+    def remove_special_characters(self, text, remove_digits=False):
+        pattern = r'[^a-zA-z0-9\s]' if not remove_digits else r'[^a-zA-z\s]'
+        text = re.sub(pattern, '', text)
+        return text.lower()
+
+    def remove_stopwords(self, text, is_lower_case=False):
+        tokenizer = ToktokTokenizer()
+        with open('data/dutch', "r") as file:
+            stopword_list = [i.strip() for i in file]
+        tokens = tokenizer.tokenize(text)
+        tokens = [token.strip() for token in tokens]
+        if is_lower_case:
+            filtered_tokens = []
+            for token in tokens:
+                if token not in stopword_list:
+                    filtered_tokens.append(token)
+            # filtered_tokens = [token for token in tokens if token not in stopword_list]
+        else:
+            filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
+        filtered_text = ' '.join(filtered_tokens)
+        return filtered_text
+
+    def calc_bigrams(self):
+        bgs = bigrams(re.split('\.|\n|\s|,', self.all_text))
+        fdist = nltk.FreqDist(bgs)
+        return fdist
+
+    def count_words(self):
+        fdist = nltk.FreqDist(re.split('\.|\n|\s|,', self.all_text))
+        return fdist
+
+    def print_bigrams(self):
+        fdist = self.calc_bigrams()
+        for k, v in fdist.items():
+            print(k, v)
+
+    def print_word(self):
+        fdist = self.count_words()
+        for k, v in fdist.items():
+            print(k, v)
+
+    # def array_from_fdist(self, fdist):
+    #
+    #
+    # def x_most_occuring_bigrams(self, x):
+    #     return heapq.nlargest(x, wordfreq, key=wordfreq.get)
+
+fake = Counter()
+fake_sites = ['staopvoorvrijheid-articles.json', 'stichtingvaccinvrij-articles.json', 'transitieweb-articles.json']
+fake.add_sites(fake_sites)
+#
+# real = Counter()
+# real_sites = ['']
+# real.add_sites(real_sites)
+
+def calc_difference(fdist1, fdist2):
+    diff = list(set(fdist1.keys())-set(fdist2.keys()))
+    print(diff)
+
