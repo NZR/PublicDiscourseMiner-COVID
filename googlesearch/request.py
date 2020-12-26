2ea0e10c9e7dc94890460bee6aa879d7947e6cde
Joso <s1742450@utwente.nl>
2020-12-26 20:08:40 +0100

    Added Google Trend scaper, added google_trends.json with succesful scrapes

--- googlesearch/request.py
+++ googlesearch/request.py
@@ -1,29 +1,50 @@
 from pytrends import dailydata
 import json
 
-START_MONTH = 6 # including this month
-END_MONTH = 11  # including this month
-START_YEAR = 2020
-END_YEAR = 2020
-LANGUAGE = 'NL'
+
+# function to add to JSON
+def write_json(data, outfile):
+    with open(outfile, 'w') as f:
+        json.dump(data, f, indent=4)
 
 
-categories = ["1) Person (referred to in conspiracy theories)", "2) Person involved in fake news fabrication",
-              "3) Fakenews movement", "4) Other disease", "5) Vaccin discourage term",
-              "6) Accusatory term for corona protagonist", "8) Other (partly) complot theories"]
+def create_jsonfile(filename, categories):
+    output = {"category": {}}
+    with open(filename, 'w') as outfile:
+        json.dump(output, outfile, indent=4)
 
-wordsandbigrams = json.load(open("./input/wordsandbigrams.json"))
+    for category in categories:
+        with open(filename) as json_file:
+            data = json.load(json_file)
+            temp = data['category']
+            temp[category] = {}
+            temp[category]['word'] = {}
+            temp[category]['bigram'] = {}
+        write_json(data, filename)
 
-output = {"category": {}}
 
-for category in categories:
-    output['category'][category] = {}
-    for word in wordsandbigrams['category'][category]['word']:
-        df = dailydata.get_daily_data(word, START_YEAR, START_MONTH, END_YEAR, END_MONTH, LANGUAGE)
-        output['category'][category][word] = json.loads(df.to_json())[word]
-    # for bigram in wordsandbigrams['category'][category]['bigram']:
-    #     df2 = dailydata.get_daily_data(bigram, START_YEAR, START_MONTH, END_YEAR, END_MONTH, LANGUAGE)
-    #     output['category'][category][bigram] = json.loads(df2.to_json())[bigram]
+def trend_lookup(filename, category, word_bigram, is_word, start_year, start_month, end_year, end_month, language):
 
-with open("./output/words.json") as outfile:
-    json.dump(output, outfile)
+    print("fetching ", word_bigram)
+    df = dailydata.get_daily_data(word_bigram, start_year, start_month, end_year, end_month, language)
+
+    with open(filename) as json_file:
+        data = json.load(json_file)
+        temp = data['category'][category]
+        if is_word:
+            temp['word'][word_bigram] = json.loads(df.to_json())[word_bigram]
+        else:
+            temp['bigram'][word_bigram] = json.loads(df.to_json())[word_bigram]
+        write_json(data, filename)
+
+
+# for bigram in wordsandbigrams['category'][category]['bigram']:
+#     print("fetching ", bigram)
+#     df2 = dailydata.get_daily_data(bigram, START_YEAR, START_MONTH, END_YEAR, END_MONTH, LANGUAGE)
+#     with open(OUTFILE) as json_file:
+#         data = json.load(json_file)
+#         temp = data['category'][category]
+#         temp['bigram'][bigram] = json.loads(df2.to_json())[bigram]
+#     write_json(data)
+#     df2 = df2.iloc[0:0]
+
