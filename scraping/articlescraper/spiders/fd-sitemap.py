358977bb3ecf5529f0f86f09b7e9b46bc959ed05
JvdK <j.vanderkaaij@student.utwente.nl>
2021-02-23 15:09:31 +0100

    Remove cookies

--- scraping/articlescraper/spiders/fd-sitemap.py
+++ scraping/articlescraper/spiders/fd-sitemap.py
@@ -6,14 +6,12 @@ from scrapy import Request
 class FdSitemapSpider(scrapy.Spider):
     name = "fd-sitemap-css"
     start_urls = []
-    #TODO als er meerdere pagina's zijn met corona-links, voeg die dan toe aan start_urls. Bij de NOS was dit een simpele
-    # pagina-count met zoeken op 'corona'
-    for i in range(1, 48):
-        start_urls.append('https://fd.nl/search?customPeriod.start=2020-04-01&customPeriod.end=2020-12-13&period=custom-period&tags=Coronavirus&page=' + str(i)) #TODO pas links aan
+    for i in range(1, 48):  # Number of pages with tag "Coronavirus"
+        start_urls.append('https://fd.nl/search?customPeriod.start=2020-04-01&customPeriod.end=2020-12-13&period=custom-period&tags=Coronavirus&page=' + str(i))
 
     def start_requests(self):
         for url in self.start_urls:
-            yield Request(url, cookies={'FDSSO':'7D9A361A-F855-46E9-8EF6-99B0BD1BA130', "gdpr-dau": "true"}) #TODO zet hier cookie
+            yield Request(url, cookies={'FDSSO': 'coockie_value', "gdpr-dau": "true"})  # Add your FD cookie here
 
     def parse(self, response):
         # print(response.text)
